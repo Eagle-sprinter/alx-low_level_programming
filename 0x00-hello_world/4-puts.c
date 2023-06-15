85fc59370e570ead65e70265ef5a6ee203b9dc52
Eagle-sprinter <josephmogaka98@gmail.com>
2023-06-15 01:40:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a line using the puts function
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: 0 to indicate success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
+
+
+
+
+
+
+
+

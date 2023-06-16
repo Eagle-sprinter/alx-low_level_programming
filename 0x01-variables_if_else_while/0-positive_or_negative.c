d19107117226b7548bf4eb3e90c7c0315572cb1a
Eagle-sprinter <josephmogaka98@gmail.com>
2023-06-16 02:20:32 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(0)); // Seed the random number generator with current time
+
+    int n = rand(); // Generate a random number
+
+    printf("The number %d is ", n);
+    if (n > 0) {
+        printf("positive");
+    } else if (n == 0) {
+        printf("zero");
+    } else {
+        printf("negative");
+    }
+    printf("\n");
+
+    return 0;
+}
+

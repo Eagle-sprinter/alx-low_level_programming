54be4c60fdf9a3a0dce44815e5510ac0d8a8cabb
Eagle-sprinter <josephmogaka98@gmail.com>
2023-06-20 02:14:20 -0700

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * main - entry point
+ * Retrun: Void
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}

75dffc44366d89d85c39cec5a814af836a9ed5b3
Eagle-sprinter <josephmogaka98@gmail.com>
2023-07-28 08:53:19 -0700

    post

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,46 +1,30 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
-/**
- * _strlen - finds the length of a string
- * @str: string to find the length of
- *
- * Return: length of string
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * add_node - adds a node to the beginning of a linked list
- * @head: double pointer to a lined list
- * @str: string to add to the new node
- *
- * Return: pointer to the new node
- */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+    if (str == NULL)
+        return NULL;
 
-	if (str == NULL)
-		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(new->str);
-	new->next = *head;
-	*head = new;
-	return (new);
+    list_t *new_node; // Declare the variable at the beginning of the function
+
+    new_node = malloc(sizeof(list_t)); // Now, you can assign the value here
+
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return new_node;
 }
+

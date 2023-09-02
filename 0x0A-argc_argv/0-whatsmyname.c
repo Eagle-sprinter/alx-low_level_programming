176c484abc1e87e382e21b2a61501739d65e8c4d
Eagle-sprinter <josephmogaka98@gmail.com>
2023-09-02 10:11:23 -0700

    hello world

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,8 @@
  *
  * return:(0) when success
  */
-int main (int argc , char *argv[])
+int main (int argc __attribute__((unused)), char *argv[])
 {
-	(void)argc;
-
-	printf("%s\n", argv[0]);
+  	printf("%s\n", argv[0]);
 	return (0);
 }
